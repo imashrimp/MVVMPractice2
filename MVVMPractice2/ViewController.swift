5d8d3d61524d53b1e08a126a3f3361c3b6658bb1
imashrimp <kwonhs8287@gmail.com>
2023-09-13 20:45:31 +0900

    Initial Commit

---
+++ MVVMPractice2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMPractice2
+//
+//  Created by 권현석 on 2023/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
